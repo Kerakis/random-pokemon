0c4552d7b9e5b5becb221c0f3df13ffec5776b76
Rob Upchurch <rlupchurch3@gmail.com>
2021-06-12 17:38:30 -0400

    Added height, weight, and the generation that the pokemon was added.

--- poke.js
+++ poke.js
@@ -2,6 +2,10 @@
 const pokemonName = document.querySelector("#nameData");
 const pokemonId = document.querySelector("#idData");
 const pokemonTypes = document.querySelector("#typesData");
+const pokemonHeight = document.querySelector("#heightData");
+const pokemonWeight = document.querySelector("#weightData");
+const pokemonGen = document.querySelector("#generationData");
+// const pokemonEvolution = document.querySelector("#evolutionData");
 
 async function fetchData() {
   // Select a random pokemon. Update this to take the string length of pokemon.species
@@ -11,11 +15,25 @@ async function fetchData() {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const pokeData = await response.json();
 
+  const response2 = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}`
+  );
+  const speciesData = await response2.json();
+
   // Define the pokemon's name for the given id
   const pokeName = pokeData.name;
 
   // Define the pokemon's id
   const pokeId = pokeData.id;
+
+  // Define the pokemon's id
+  const pokeHeight = pokeData.height;
+
+  // Define the pokemon's id
+  const pokeWeight = pokeData.weight;
+
+  // Define the pokemon's generation
+  const pokeGen = speciesData.generation.name;
 
   // Define the pokemon's type(s)
   const poke1Type = pokeData.types[0].type.name;
@@ -28,11 +46,31 @@ async function fetchData() {
     pokemonTypes.innerText = `${poke1Type}`;
   }
 
+  // Define the pokemon's evolution- WIP
+  // const pokeEvolution = speciesData.evolves_from_species;
+  // console.log(pokeEvolution);
+  // const pokeEvolutionName = speciesData.evolves_from_species.name;
+  // console.log(pokeEvolutionName);
+  // if (pokeEvolution !== "null") {
+  //   pokemonEvolution.innerText = `This pokemon evolves from ${pokeEvolutionName}`;
+  // } else {
+  //   pokemonEvolution.innerText = ``;
+  // }
+
   // Display the pokemon's name
   pokemonName.innerText = pokeName;
 
   // Display the pokemon's id
   pokemonId.innerText = pokeId;
+
+  // Display the pokemon's height
+  pokemonHeight.innerText = pokeHeight;
+
+  // Display the pokemon's weight
+  pokemonWeight.innerText = pokeWeight;
+
+  // Display the pokemon's generation
+  pokemonGen.innerText = pokeGen;
 
   // Display the pokemon's sprite
   const pokeImage = pokeData.sprites.front_default;
